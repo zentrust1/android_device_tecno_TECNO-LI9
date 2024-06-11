301ccf3099b4d4932da3aae0b19e4d53df856eba
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-11 12:46:56 +0200

    TECNO-LI9: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO LI9 Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for TECNO TECNO LI9 (TECNO-LI9)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_TECNO-LI9.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-LI9 device
+$(call inherit-product, device/tecno/TECNO-LI9/device.mk)
+
+PRODUCT_DEVICE := TECNO-LI9
+PRODUCT_NAME := omni_TECNO-LI9
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO LI9
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_li9_h335-user 12 SP1A.210812.016 617037 release-keys"
+
+BUILD_FINGERPRINT := TECNO/LI9-OP/TECNO-LI9:12/SP1A.210812.016/240523V1543:user/release-keys
