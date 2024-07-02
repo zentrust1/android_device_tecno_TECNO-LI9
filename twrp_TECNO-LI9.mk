281a52aff1c53e488f42d9c7c4d657383a7e9a41
zentrust1 <146930369+zentrust1@users.noreply.github.com>
2024-07-02 16:09:17 +0700

    Update and rename omni_TECNO-LI9.mk to twrp_TECNO-LI9.mk

--- twrp_TECNO-LI9.mk
+++ twrp_TECNO-LI9.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-LI9 device
 $(call inherit-product, device/tecno/TECNO-LI9/device.mk)
 
 PRODUCT_DEVICE := TECNO-LI9
-PRODUCT_NAME := omni_TECNO-LI9
+PRODUCT_NAME := twrp_TECNO-LI9
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LI9
 PRODUCT_MANUFACTURER := tecno
