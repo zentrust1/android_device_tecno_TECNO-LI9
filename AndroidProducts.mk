273e7ae7820962b37125ba79f661b329eb5e6e78
zentrust1 <146930369+zentrust1@users.noreply.github.com>
2024-07-02 16:43:24 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_TECNO-LI9.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_TECNO-LI9-user \
-    omni_TECNO-LI9-userdebug \
-    omni_TECNO-LI9-eng
+    twrp_TECNO-LI9-user \
+    twrp_TECNO-LI9-userdebug \
+    twrp_TECNO-LI9-eng
